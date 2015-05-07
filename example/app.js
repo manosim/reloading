b9aee1cba3736a903cc5accf02a9cf2fa69ead7d
Emmanouil Konstantinidis <manos@iamemmanouil.com>
2015-05-07 19:29:52 +0100

    Loading Component

--- example/app.js
+++ example/app.js
@@ -2,11 +2,15 @@
  * @jsx React.DOM
  */
 
+var React = require('react');
+var Loading = require('../');
+
 var App = React.createClass({
   render: function() {
     return (
       <div>
         Hello, world! I am a React thing.
+        <Loading shouldShow={true} text='Loading... Give it a minute!' />
       </div>
     );
   }
